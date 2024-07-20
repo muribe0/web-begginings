1be78f79d97e368a66f56d061d57f19bc2ace3ae
muribe0 <muribe@fi.uba.ar>
2024-07-20 04:52:07 -0300

    added the function to add a new task for the website using django forms, an object, ...

--- Django/lecture3/tasks/urls.py
+++ Django/lecture3/tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = "tasks"
 urlpatterns = [
     path("", views.index, name="index"), # This is a path that will be used to access the index function in the views.py file
     path("add", views.add, name="add"),
--- Django/lecture3/tasks/views.py
+++ Django/lecture3/tasks/views.py
@@ -1,6 +1,13 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django import forms
+from django.urls import reverse
 
-tasks = ["foo", "bar", "baz"]
+tasks = []
+
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
 
 
 # Create your views here.
@@ -11,4 +18,22 @@ def index(request):
 
 
 def add(request):
-    return render(request, "tasks/add.html")
+    if request.method == "POST":  # SERVER-SIDE VALIDATION
+        form = NewTaskForm(request.POST)  # request.POST contains all the data that the user submitted.
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))  # Redirects the user to the index page.
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form,
+            })
+
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm(), # creates a new instance of the NewTaskForm class.
+    })
+
+def add_form_not_working_yet(request):
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm(), # creates a new instance of the NewTaskForm class.
+    })
